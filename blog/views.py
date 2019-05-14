b64929f784c57f40086c0d4ca861e6a3544120c1
sunil <kslvsunil@gmail.com>
2019-05-14 21:34:15 +0200

    added static, templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
-# Create your views here.
+posts = [
+    {
+        'author': 'Sunil',
+        'title': 'Blog post 1',
+        'content': 'First post',
+        'date_posted': 'May 10'
+
+    },
+    {
+        'author': 'Sekar',
+        'title': 'Blog post 2',
+        'content': 'Second post',
+        'date_posted': 'May 11'
+
+    }
+]
+
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
