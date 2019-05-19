2390b1213762b8006e9b29bb4870b6957de84369
sunil <kslvsunil@gmail.com>
2019-05-19 06:22:47 +0200

    created users app

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Sunil',
-        'title': 'Blog post 1',
-        'content': 'First post',
-        'date_posted': 'May 10'
-
-    },
-    {
-        'author': 'Sekar',
-        'title': 'Blog post 2',
-        'content': 'Second post',
-        'date_posted': 'May 11'
-
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
